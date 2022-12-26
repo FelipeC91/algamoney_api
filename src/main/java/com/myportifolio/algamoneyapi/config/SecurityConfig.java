10f1c804d83a5d07c4bffa52824ac3d6bef723a8
FelipeC91 <feelipecampos91@gmail.com>
2022-12-26 13:49:31 -0300

    progresso aula 6.1

---
+++ src/main/java/com/myportifolio/algamoneyapi/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.myportifolio.algamoneyapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    protected InMemoryUserDetailsManager userDetailsService() {
+        var encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername("admin")
+                .password(encoder.encode("admin"))
+                .roles("ROLE")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    protected SecurityFilterChain filterChain(AuthenticationManagerBuilder authenticationManagerBuilder,HttpSecurity http) throws Exception {
+        http.httpBasic()
+                    .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                .csrf().disable()
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/categoria").permitAll()
+                                .anyRequest().authenticated()
+
+                );
+
+        return http.build();
+    }
+
+}
