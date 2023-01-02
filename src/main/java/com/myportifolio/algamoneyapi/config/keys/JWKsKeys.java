9cde5ee37877bdc57fb1f14d8fc089cb984cee1a
FelipeC91 <feelipecampos91@gmail.com>
2023-01-02 14:21:46 -0300

    yt

---
+++ src/main/java/com/myportifolio/algamoneyapi/config/keys/JWKsKeys.java
@@ -0,0 +1,29 @@
+package com.myportifolio.algamoneyapi.config.keys;
+
+import com.nimbusds.jose.jwk.RSAKey;
+
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+public class JWKsKeys {
+    public static RSAKey generate(){
+        try {
+            var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            var keyPair = keyPairGenerator.generateKeyPair();
+
+            var publicKey = (RSAPublicKey) keyPair.getPublic();
+            var privateKey = (RSAPrivateKey) keyPair.getPrivate();
+
+            return new RSAKey.Builder(publicKey)
+                    .privateKey(privateKey)
+                    .keyID(UUID.randomUUID().toString())
+                    .build();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Problem generating RSA Keys",e);
+        }
+
+    }
+}
